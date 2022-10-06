f440a1cd857279f02fea55aebb2cb2ea90503212
Victor I. Afolabi <javafolabi@gmail.com>
2022-10-06 15:46:35 +0100

    Create image generation pipeline

--- src/text_to_image/generate.py
+++ src/text_to_image/generate.py
@@ -1,20 +1,39 @@
+# Copyright 2022 Victor I. Afolabi
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import torch
 from diffusers import StableDiffusionPipeline
 from PIL import Image
+from text_to_image.models import decode_img_latents
+from text_to_image.models import get_text_embeddings
+from text_to_image.models import produce_latents
 from torch import autocast  # mypy: allow-implicit-reexport
 
 
-def generate(prompt: str) -> Image.Image:
+def generate(
+    prompts: str | list[str],
+    steps: int = 50,
+) -> Image.Image:
     """Generate image from text prompt.
 
     Args:
-        prompt (str): Image text description.
+        prompt (str | list[str]): Image text description.
 
     Returns:
         Image.Image - A Pillow Image object.
     """
-    # if isinstance(prompt, str):
-    #     prompt = [prompt]
+    if isinstance(prompts, str):
+        prompts = [prompts]
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -26,6 +45,48 @@ def generate(prompt: str) -> Image.Image:
     pipe = pipe.to(device)
 
     with autocast(device):
-        image: Image.Image = pipe(prompt)['sample'][0]
+        image: Image.Image = pipe(
+            prompts,
+            num_inference_steps=steps,
+        )['sample'][0]
 
     return image
+
+
+def generate_images(
+    prompts: str | list[str],
+    height: int = 512,
+    width: int = 512,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5,
+    latents: torch.Tensor | None = None,
+) -> list[Image.Image]:
+    """Generate image(s) from text prompt(s).
+
+    Args:
+        prompts (str | list[str]): Text prompt(s).
+        height (int, optional): Generated image height. Defaults to 512.
+        width (int, optional): Generated image width. Defaults to 512.
+        num_inference_steps (int, optional): Number of inference steps. Defaults to 50.
+        guidance_scale (float, optional): How much to prefix affects output. Defaults to 7.5.
+        latents (torch.Tensor | None, optional): Image latent embeddings. Defaults to None.
+
+    Returns:
+        list[Image.Image]: Image or list of generated PIL images.
+    """
+    # Prompts -> text embeddings.
+    text_embeddings = get_text_embeddings(prompts)
+
+    # Text embeddings -> image latents.
+    latents = produce_latents(
+        text_embeddings,
+        height=height, width=width,
+        latents=latents,
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
+    )
+
+    # Image latents -> Images.
+    imgs = decode_img_latents(latents)
+
+    return imgs
--- src/text_to_image/models.py
+++ src/text_to_image/models.py
@@ -11,3 +11,205 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import torch
+from diffusers import AutoencoderKL
+from diffusers import LMSDiscreteScheduler
+from diffusers import UNet2DConditionModel
+from PIL import Image
+from torch import autocast  # mypy: allow-implicit-reexport
+from torch import nn
+from tqdm import tqdm
+from transformers import CLIPTextModel
+from transformers import CLIPTokenizer
+
+
+# Pretrained models.
+PRETRAINED_IMAGE_MODEL = 'CompVis/stable-diffusion-v1-4'
+PRETRAINED_TEXT_MODEL = 'openai/clip-vit-large-patch14'
+SCHEDULER_TRAIN_STEPS = 1000
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def get_text_embeddings(
+    text: str | list[str],
+    prefix: str | list[str] = '',
+    tokenizer: nn.Module | None = None,
+    text_encoder: nn.Module | None = None,
+) -> torch.Tensor:
+    """Get text embeddings from text prompt.
+
+    Args:
+        text (str | list[str]): Text prompt.
+        prefix (str | list[str], optional): Prefix to add to text prompt.
+            Defaults to ''.
+        tokenizer (nn.Module, optional) - Pre-trained text tokenizer.
+            Defaults to `transformers.CLIPTokenizer`.
+        text_encoder (nn.Model, optional) - Pre-trained text encoder.
+            Defaults to `transformers.CLIPTextModel`.
+
+    Returns:
+        torch.Tensor: Text embeddings.
+    """
+    if isinstance(text, str):
+        text = [text]
+
+    if not tokenizer:
+        # Load the tokenizer to tokenize the text prompt..
+        tokenizer = CLIPTokenizer.from_pretrained(
+            PRETRAINED_TEXT_MODEL,
+        )
+
+    # Tokenize the text prompt.
+    tokens = tokenizer(
+        text, padding=True,  # padding='max_length',
+        max_length=tokenizer.model_max_length,
+        truncation=True, return_tensors='pt',
+    )
+
+    if not text_encoder:
+        # Load the encoder to encode the text prompt into embeddings.
+        text_encoder = CLIPTextModel.from_pretrained(
+            PRETRAINED_TEXT_MODEL,
+        )
+        text_encoder = text_encoder.to(DEVICE)
+
+    with torch.no_grad():
+        embeddings = text_encoder(tokens.input_ids.to(DEVICE))[0]
+
+    if isinstance(prefix, str):
+        prefix = [prefix] * len(text)
+
+    # Embed the prefix into the text embeddings.
+    prefix_tokens = tokenizer(
+        prefix, padding=True,  # padding='max_length',
+        max_length=tokenizer.model_max_length,
+        return_tesnsors='pt',
+    )
+    with torch.no_grad():
+        prefix_embeddings = text_encoder(
+            prefix_tokens.input_ids.to(DEVICE),
+        )[0]
+
+    # Concatenate the prefix and text embeddings.
+    text_embeddings = torch.cat([prefix_embeddings, embeddings])
+
+    return text_embeddings
+
+
+def produce_latents(
+    text_embeddings: torch.Tensor,
+    height: int = 512, width: int = 512,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5,
+    latents: torch.Tensor | None = None,
+    img_model: nn.Module | None = None,
+    scheduler: LMSDiscreteScheduler | None = None,
+) -> torch.Tensor:
+    """Produce latent space from text embeddings.
+
+    Args:
+        text_embeddings (torch.Tensor): Text embeddings.
+        height (int, optional): Height of latent image. Defaults to 512.
+        width (int, optional): Width of latent image. Defaults to 512.
+        num_inference_steps (int, optional): Number of inference steps.
+            Defaults to 50.
+        guidance_scale (float, optional): How much guidance should text prefix
+            affect the output. Defaults to 7.5.
+        latents (torch.Tensor | None, optional): Latent embedding.
+            Defaults to None.
+        img_model (nn.Module | None, optional): Image model to predict noise
+            (UNet model). Defaults to None.
+        scheduler (LMSDiscreteScheduler | None, optional): Scheduler for
+            inference. Defaults to None.
+
+    Returns:
+        torch.Tensor: Latent embeddings representing the text prompt as well
+            as image noise.
+    """
+    if img_model is None:
+        # Load the UNet model for generating latents.
+        img_model = UNet2DConditionModel.from_pretrained(
+            PRETRAINED_IMAGE_MODEL,
+            subfolder='unet',
+            use_auth_token=True,
+        )
+
+    if latents is None:
+        # Generate random latent noise.
+        latents = torch.randn((
+            text_embeddings.shape[0] // 2,
+            img_model.in_channels,
+            height // 8, width // 8,
+        ))
+    latents = latents.to(DEVICE)
+
+    if scheduler is None:
+        scheduler = LMSDiscreteScheduler(
+            beta_start=0.00085, beta_end=0.012,
+            beta_schedule='scaled_linear',
+            num_train_timesteps=SCHEDULER_TRAIN_STEPS,
+        )
+    scheduler.set_timesteps(num_inference_steps)
+    latents *= scheduler.sigmas[0]
+
+    with autocast(DEVICE):
+        for i, t in tqdm(enumerate(scheduler.timesteps)):
+            # Expand the latents if we are doing classifier-free guidance
+            # to avoid doing two forward passes.
+            latent_model_input = torch.cat([latents] * 2)
+            sigma = scheduler.sigmas[i]
+            latent_model_input /= ((sigma ** 2 + 1) ** 0.5)
+
+            # Predict the noise residual.
+            with torch.no_grad():
+                noise_pred = img_model(
+                    latent_model_input, t,
+                    encoder_hidden_states=text_embeddings,
+                )['sample']
+
+            # Perform guidance
+            noise_pred_prefix, noise_pred_text = noise_pred.chunk(2)
+            noise_pred = (
+                noise_pred_prefix + guidance_scale *
+                (noise_pred_text - noise_pred_prefix)
+            )
+
+            # Compute the previous noisy sample x_t -> x_t-1
+            latents = scheduler.step(noise_pred, i, latents)['prev_sample']
+
+    return latents
+
+
+def decode_img_latents(
+    latents: torch.Tensor,
+    decoder: AutoencoderKL | None = None,
+) -> list[Image.Image]:
+    """Decode latent embeddings into images.
+
+    Args:
+        latents (torch.Tensor): Latent embeddings.
+        decoder (AutoencoderKL | None, optional): Pre-trained
+            decoder to decode latent embeddings. Defaults to None.
+
+    Returns:
+        list[Image.Image]: List of decoded images as a PIL image.
+    """
+    latents = 1 / 0.18215 * latents
+
+    if decoder is None:
+        decoder = AutoencoderKL.from_pretrained(
+            PRETRAINED_IMAGE_MODEL,
+            subfolder='decoder',
+            use_auth_token=True,
+        )
+        decoder = decoder.to(DEVICE)
+
+    with torch.no_grad():
+        imgs = decoder.decode(latents)['sample']
+
+    imgs = (imgs / 2 + 0.5).clamp(0, 1)
+    imgs = imgs.detach().cpu().permute(0, 2, 3, 1).numpy()
+    imgs = (imgs * 255).round().astype('uint8')
+
+    pil_imgs = [Image.fromarray(img) for img in imgs]
+    return pil_imgs
